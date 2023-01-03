95a8ef8b0d5739c4c2219838ed6fc60c03a0fb0b
Jens-Mikael Stjernberg <jensmikael.stjernberg@gmail.com>
2023-01-03 13:47:40 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+//own components
+import SearchBar from "./components/SearchBar";
+import AppToolBar from "./components/AppToolBar";
+
+//MUI components
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+
+//import useStyles from "./styles";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <main>
+        <AppToolBar />
+        <Typography variant="h3" align="center">
+          Weather app
+        </Typography>
+        <SearchBar />
+      </main>
+    </>
   );
-}
-
+};
 export default App;
